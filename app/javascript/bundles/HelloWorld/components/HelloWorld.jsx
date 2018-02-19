505e18b28f96ff2b9edc577c8fd26458b2c9bc58
pbrudny <pbrudny@gmail.com>
2018-02-19 18:47:06 +0100

    add redux

--- app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default class HelloWorld extends React.Component {
   static propTypes = {
-    name: PropTypes.string.isRequired // this is passed from the Rails view
+    name: PropTypes.string.isRequired, // this is passed from the Rails view
   };
 
   /**
